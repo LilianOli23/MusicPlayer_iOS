f853363e7d5a1134eeea6416f1e6c0863ce25620
liliandeoliveirasilva <lilian.agricola@gmail.com>
2022-05-24 14:25:42 -0300

    Initial Commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Lilian De Oliveira Silva on 10/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
